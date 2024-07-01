ce897f443b22bb0923a38a0400d16acb0860ddb0
AuricelioJunior9 <juniorfernandesrw9@gmail.com>
2024-07-01 19:01:28 -0300

    Criado classes do projeto de enumeração

---
+++ src/entities/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package entities.enums;
+
+public enum OrderStatus {
+    PENDING_PAYMENT, //aguardando o pagamento
+    PROCESSING, //processando
+    SHIPPED, //enviado
+    DELIVERED, //entrege
+    
+}
